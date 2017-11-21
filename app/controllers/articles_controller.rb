3cbfa3e0386e81f2c798f4016c68788e7f8a3b84
hortense martin <hortense.mdf@gmail.com>
2017-11-21 15:03:09 +0100

    Creating a Wikipedia with seed Faker

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+  before_action :load_article, only: [:edit, :update, :destroy, :show]
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    Article.create(article_params)
+  end
+
+  def update
+    @article.update_attributes(article_params)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+private
+
+  def load_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+end
